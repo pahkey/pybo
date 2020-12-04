2727465ca4b04401c536bc6410e6074bae66e58d
박응용 <pahkey@gmail.com>
2020-12-04 12:55:30 +0900

    404

--- common/urls.py
+++ common/urls.py
@@ -10,5 +10,3 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', views.signup, name='signup'),
 ]
-
-handler404 = 'common.views.page_not_found'
